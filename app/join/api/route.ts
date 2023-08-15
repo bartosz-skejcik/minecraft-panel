0ad79772278582d30de696bae2c1eda5200ee420
Bartek Paczesny <bartek@paczesny.pl>
2023-08-15 22:51:24 +0200

    Initial commit

---
+++ app/join/api/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/db";
+
+export async function POST(req: Request) {
+    // get the body of the request
+    const { name, uuid, globalTime, localTime }: any = await req.json();
+
+    // add join to the database
+    const joinLog = await prisma.joinLog.create({
+        data: {
+            name,
+            uuid,
+            globalTime,
+            localTime,
+        },
+    });
+
+    console.log("Join log added to database\n", joinLog);
+
+    // return the response
+    return NextResponse.json(joinLog);
+}
